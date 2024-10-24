d239ef8db8fcafbfe9b7248afb75b0f83aa869e2
PedroAugMedeiros <pedroaugmed7@gmail.com>
2024-10-23 21:13:23 -0300

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { PokedexContextProvider } from "./context/PokedexContext";
+import { PokedexContextProvider } from "./context/PokedexContext";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
